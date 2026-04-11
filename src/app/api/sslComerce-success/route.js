82e2d8ee9be1d19b6dbd944c095a63bf8c9f1bf1
Md. Sazzad Hossen <mdsazzadhosen472@gmail.com>
2026-04-12 03:40:52 +0600

    added demo ssl payment method

--- src/app/api/sslComerce-success/route.js
+++ src/app/api/sslComerce-success/route.js
@@ -1,6 +1,9 @@
 // Store ID: sazza69d51f3370864
 // Store Password (API/Secret Key): sazza69d51f3370864@ssl
 
+import dbConnect from "@/app/lib/dbConnect";
+import axios from "axios";
+
 
 // Merchant Panel URL: https://sandbox.sslcommerz.com/manage/ (Credential as you inputted in the time of registration)
 
@@ -16,10 +19,29 @@
 export async function POST(req) {
   try {
     const body = await req.formData();
-    console.log("SSL Success Data:", body);
+    const orderCollection = await dbConnect('purchasesItems');
+
+    const isPaymentSuccess = await axios.get(
+      `https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?val_id=${body.get("val_id")}&store_id=sazza69d51f3370864&store_passwd=sazza69d51f3370864@ssl&v=1&format=json`
+    );
+
+    console.log("Payment Validation Result:", isPaymentSuccess.data);
+    if (isPaymentSuccess.data.status !== "VALID") {
+        return Response.json({
+          message: "Payment validation failed",
+        }, { status: 400 });
+    }
+      // ✅ Payment is valid, you can update order status in your database here
+
+      const updateOrderStatus = await orderCollection.updateOne(
+        { transactionId: isPaymentSuccess.data.tran_id },
+        { $set: { status: "paid" } }
+      );
+    
 
     return Response.json({
       message: "Payment success handled",
+      
     });
   } catch (error) {
     return Response.json(
